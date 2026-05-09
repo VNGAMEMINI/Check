ea321ed8895e9ad646b79175153ecef747994257
VNGAME <pnsong2707@gmail.com>
2026-05-09 21:13:06 +0700

    j

--- Check/src/main.jsx
+++ Check/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 import data from "./json/test.json";
@@ -7,9 +7,9 @@ import Poser from "./element/poser.jsx";
 const len = data.length;
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <HashRouter>
     <>
       <Poser data={data} title_h1={"trang web"} title_p={`có ${len} câu hỏi`} />
     </>
-  </StrictMode>,
+  </HashRouter>,
 );
